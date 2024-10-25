8aa1b7f45b91627f5fc08cca624c482551d3d49d
Daniel James <futathesis@gmail.com>
2024-10-25 14:20:00 +0100

    commit1025240219

---
+++ requirements.R
@@ -0,0 +1,12 @@
+# List of required packages
+required_packages <- c(
+  "pacman"
+)
+
+# Load the required packages
+pacman::p_load(forecast)
+pacman::p_load(reset_test)
+pacman::p_load(ggplot2)
+pacman::p_load(dplyr)
+pacman::p_load(lubridate)
+pacman::p_load(tseries)
